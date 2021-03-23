844decd25cd1bc98549f7fd2729aca69891fe09e
Konstantin Barabanov <criitch@yandex.ru>
2021-03-23 17:32:35 +0300

    chore: dep @web-bee-ru/commitlint-config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,2 +1,15 @@
-const { default: conf } = require('./build/commitlint.config.js');
-module.exports = conf;
+const makeConfig = require('@web-bee-ru/commitlint-config');
+
+module.exports = makeConfig({
+  issuePrefixes: [
+    'QWE-',
+    'ASD-',
+  ],
+  commitTypes: [
+    'feat', // @NOTE: implementation of functionality
+    'fix', // @NOTE: bugfix
+    'wip', // @NOTE: work in progress
+    'ci', // @NOTE: continuous integration related issues
+    'chore', // @NOTE: should be moved in the starter-project
+  ],
+});
